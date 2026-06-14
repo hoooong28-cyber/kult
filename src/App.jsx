0ba04eb8e4c944d0f1aff016f0093f3925685660
이재홍 <hoo__oong@ijaehong-ui-MacBookAir.local>
2026-06-14 19:38:15 +0900

    Auto-save

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import SearchResults from './pages/SearchResults'
 import CreditCheckout from './pages/CreditCheckout'
 import SpaceDetail from './pages/SpaceDetail'
-import SectorSelector from './pages/SectorSelector'
-import Join from './pages/Join'
-import Admin from './pages/Admin'
 import Subscribe from './pages/Subscribe'
 import { LanguageProvider } from './context/LanguageContext'
 
@@ -14,18 +10,10 @@ function App() {
         <LanguageProvider>
             <Router>
                 <Routes>
-                    {/* Full-layout pages (have their own header/footer) */}
                     <Route path="/" element={<Home />} />
-                    <Route path="/search" element={<SearchResults />} />
-                    <Route path="/search/:sector" element={<SearchResults />} />
                     <Route path="/space/:id" element={<SpaceDetail />} />
-                    <Route path="/join" element={<Join />} />
-
-                    {/* Shared-layout pages */}
                     <Route path="/credits" element={<CreditCheckout />} />
                     <Route path="/subscribe" element={<Subscribe />} />
-                    <Route path="/sectors" element={<SectorSelector />} />
-                    <Route path="/admin" element={<Admin />} />
                 </Routes>
             </Router>
         </LanguageProvider>
